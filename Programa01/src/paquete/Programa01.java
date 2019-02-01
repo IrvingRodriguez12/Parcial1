f5fa04132523c4e5ac12e115f6817f5067ae7205
PC <PC@LAPTOP-5S8L0UB3>
2019-01-31 23:54:25 -0600

    programa01

---
+++ Programa01/src/paquete/Programa01.java
@@ -0,0 +1,14 @@
+package paquete;
+
+public class Programa01 {
+
+	public Programa01() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hola mundo");
+	}
+
+}
